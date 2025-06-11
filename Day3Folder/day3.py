112626942612ae23ab176e077c44a12c1250a591
guyfromasia37 <justinahx10@gmail.com>
2025-06-11 02:29:25 +0000

    Jun 11, 2025, 2:29 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -19,7 +19,7 @@ name='Hitler'
 title='Dictator'
 command='commands the soilders to Invade Poland'
 
-(name)
+(title+name+command)
 ########################################################################
 # Task 3:
 
