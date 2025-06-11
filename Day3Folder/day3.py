21a2c2122d7c3fcc0ad0fb3d2d9f3aea1ede505f
guyfromasia37 <justinahx10@gmail.com>
2025-06-11 02:11:35 +0000

    Jun 11, 2025, 2:11 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -9,7 +9,7 @@ YOURNAME=("what is your name?")
 print("Hello"+YOURNAME)
 yourage= input("how old are you?")
 yourage= int(yourage)
-print("I am" + str(yourage))
+print("I am" + str(yourage)+" ")
 
 
 
